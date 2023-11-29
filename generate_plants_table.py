e8bbc5b7998939f94883644f1c71fd8e89aa6f0b
Developing Human <developing-human@protonmail.com>
2023-11-29 18:05:21 -0500

    Add __name__ check to generate_plants_table.py

--- generate_plants_table.py
+++ generate_plants_table.py
@@ -3,20 +3,21 @@ import luigi
 from tasks.tables.plants.generate import GeneratePlantsCsv
 import logging
 
-if len(sys.argv) != 2:
-    print(f"Usage: python3 {sys.argv[0]} plants_list.txt")
-    exit(1)
-
 logging.getLogger().setLevel(logging.WARN)
 
-result = luigi.build(
-    [GeneratePlantsCsv(plants_filename=sys.argv[1])],
-    workers=1,
-    local_scheduler=True,
-    log_level="WARNING",
-)
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print(f"Usage: python3 {sys.argv[0]} plants_list.txt")
+        exit(1)
 
-if result:
-    print("Done :)")
-else:
-    print("Done, but :(")
+    result = luigi.build(
+        [GeneratePlantsCsv(plants_filename=sys.argv[1])],
+        workers=1,
+        local_scheduler=True,
+        log_level="WARNING",
+    )
+
+    if result:
+        print("Done :)")
+    else:
+        print("Done, but :(")
