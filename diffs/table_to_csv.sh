0b295c6faa34a9269383d79e68f6cf2a24127bc6
Developing Human <developing-human@protonmail.com>
2024-02-18 09:31:25 -0500

    Cleanup table.csv shortly after creation

--- diffs/table_to_csv.sh
+++ diffs/table_to_csv.sh
@@ -12,12 +12,15 @@ table="$1"
 # Read the db password
 db_root_pw=`pass local-planting-life-db-root`
 
-# Remove the file if it exists
-# SELECT INTO OUTFILE doesn't like to overwrite
-docker exec planting_life_db rm -f /var/lib/mysql/planting_life/table.csv
-
 # create csv in /var/lib/mysql/planting_life/table.csv
 echo "SELECT * INTO OUTFILE 'table.csv' FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' LINES TERMINATED BY '\n' FROM $table" | mysql -h 127.0.0.1 -P 3306 -u root planting_life --password=$db_root_pw
 
-# write the csv to stdout, replace \" with "" to more properly escape
-docker exec planting_life_db cat /var/lib/mysql/planting_life/table.csv | sed 's/\\"/\"\"/g'
+# write the csv to a temp file, replace \" with "" to more properly escape
+tmp_file=`mktemp`
+docker exec planting_life_db cat /var/lib/mysql/planting_life/table.csv | sed 's/\\"/\"\"/g' > $tmp_file
+
+# remove table.csv from the docker container, this makes it cleaner
+# to drop the database (ex: when loading prod into local)
+docker exec planting_life_db rm /var/lib/mysql/planting_life/table.csv
+
+cat $tmp_file
